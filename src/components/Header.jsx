8176512c6d4a832012ae003013c95bec4ece849d
Daemonlite <khemikal2016@gmail.com>
2023-07-11 09:47:40 -0700

    ---

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,32 +6,63 @@ import { useNavigate } from 'react-router-dom';
 
 export default function ScrollableTabsButtonForce() {
   const [value, setValue] = React.useState(0);
+  const [isMobile, setIsMobile] = React.useState(false);
 
   const handleChange = (event, newValue) => {
     setValue(newValue);
   };
-const navigate = useNavigate()
+
+  const navigate = useNavigate();
+
+  React.useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 768);
+    };
+
+    handleResize();
+
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
   return (
- <div className='header'>
-
-   <p className='headText'>Daemonlite</p>
-    <Box sx={{ maxWidth: { xs: 320, sm: 480,marginLeft:"450px",marginTop:"-60px" }, bgcolor: 'background.paper' }}>
-      
-      <Tabs
-        value={value}
-        onChange={handleChange}
-        variant="scrollable"
-        scrollButtons
-        allowScrollButtonsMobile
-        aria-label="scrollable force tabs example"
+    <div className='header'>
+      <p className='headText'>Daemonlite</p>
+      <Box
+        sx={{
+          maxWidth: { xs: 320, sm: 480, marginLeft: '450px', marginTop: '-60px' },
+          bgcolor: 'background.paper',
+        }}
       >
-        <Tab label="Services" onClick={()=> navigate('/')} />
-        <Tab label="Products" onClick={()=> navigate('/products')}/>
-        <Tab label="Blog" onClick={()=> navigate('/blog')}/>
-      </Tabs>
-    </Box>
-   
-
- </div>
+        {isMobile ? (
+          <Tabs
+            value={value}
+            onChange={handleChange}
+            variant="scrollable"
+            scrollButtons
+            allowScrollButtonsMobile
+            aria-label="scrollable force tabs example"
+            centered
+          >
+            <Tab label="Services" onClick={() => navigate('/')} />
+            <Tab label="Products" onClick={() => navigate('/products')} />
+            <Tab label="Blog" onClick={() => navigate('/blog')} />
+          </Tabs>
+        ) : (
+          <Tabs
+            value={value}
+            onChange={handleChange}
+            variant="scrollable"
+            scrollButtons
+            allowScrollButtonsMobile
+            aria-label="scrollable force tabs example"
+          >
+            <Tab label="Services" onClick={() => navigate('/')} />
+            <Tab label="Products" onClick={() => navigate('/products')} />
+            <Tab label="Blog" onClick={() => navigate('/blog')} />
+          </Tabs>
+        )}
+      </Box>
+    </div>
   );
 }
