0e140018385195167df4003284c91f27f3748c93
Daemonlite <khemikal2016@gmail.com>
2023-07-13 18:47:30 -0700

    changes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -36,20 +36,20 @@ const navigate = useNavigate()
     <nav>
     <nav className="navbar navbar-expand-lg bg-body-tertiary">
   <div className="container-fluid">
-    <a className="navbar-brand" href="#">Daemonlite</a>
+    <a className="navbar-brand" onClick={() => navigate('/')}>Daemonlite</a>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
     <div className="collapse navbar-collapse" id="navbarNav">
       <ul className="navbar-nav">
         <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="/">Services</a>
+          <a className="nav-link active" aria-current="page" onClick={() => navigate('/')}>Services</a>
         </li>
         <li className="nav-item">
-          <a className="nav-link" href="/products">Products</a>
+          <a className="nav-link" onClick={() => navigate('/products')}>Products</a>
         </li>
         <li className="nav-item">
-          <a className="nav-link" href="/blog">Blog</a>
+          <a className="nav-link" onClick={() => navigate('/blog')}>Blog</a>
         </li>
 
 
