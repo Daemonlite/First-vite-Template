0750f08143c4e5b5701ebcb963a7422b90738ca4
Daemonlite <khemikal2016@gmail.com>
2023-07-13 18:31:31 -0700

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,8 @@ import {BrowserRouter,Routes,Route} from 'react-router-dom'
 import Services from "./components/Services"
 import Products from "./components/Products"
 import Blog from "./components/Blog"
-import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.js';
 
 function App() {
 
